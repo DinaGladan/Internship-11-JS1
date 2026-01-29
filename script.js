7bef4fc4efc31c7fb19bb4dd6adc118d16340514
DinaGladan <dina.gladan@gmail.com>
2026-01-29 17:40:33 +0100

    Added operations

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ let isOn = true;
 let isShift = true;
 let currentValue = "";
 
+let firstValue = null;
+let secondValue = null;
+let currentOperation = null;
+
 const keys = [
     {label:"0", type:"number", value:0},
     {label:"1", type:"number", value:1},
@@ -21,7 +25,13 @@ const keys = [
     {label:"7", type:"number", value:7},
     {label:"8", type:"number", value:8},
     {label:"9", type:"number", value:9},
-    {label:"C", type:"action", action:"clear"}
+    {label:"+", type:"operation", operation:"add"},
+    {label:"-", type:"operation", operation:"sub"},
+    {label:"*", type:"operation", operation:"mul"},
+    {label:"/", type:"operation", operation:"div"},
+    {label:"^2", type:"operation", operation:"square"},
+    {label:"C", type:"action", action:"clear"},
+    {label:"=", type:"equals"}
 ]
 
 function resetCalculator(){
@@ -60,14 +70,98 @@ function handleKeyPress(key){
     if(key.type ==="number"){
         handleNumber(key.value);
     }
+
+    if(key.type ==="operation"){
+        handleOperation(key.operation);
+    }
+
+    if(key.type ==="equals"){
+        handleCalculation();
+    }
+
     if(key.type ==="action" && key.action == "clear"){
         clearInput();
     }
 }
 
 function handleNumber(num){
-    currentValue +=num;
-    displayEl.textContent = currentValue;
+    if(currentOperation === null){
+        currentValue +=num;
+        displayEl.textContent = currentValue;
+    }
+    else{
+        if(secondValue === null){
+            secondValue ="";
+        }
+        secondValue += num;
+        displayEl.textContent = secondValue;
+        expressionEl.textContent =`${firstValue} ${opSymbol(currentOperation)} ${secondValue}`;
+    }
+}
+
+function handleOperation(operation){
+    if(currentValue === "" || currentOperation !== null)
+        return;
+
+    firstValue = Number(currentValue);
+    currentOperation = operation;
+    currentValue = "";
+
+    expressionEl.textContent = operation === "square" ? `${firstValue}^2` : `${firstValue} ${opSymbol(operation)}`;
+
+}
+
+function opSymbol(operation){
+    if(operation ==="add"){
+        return "+";
+    }
+    else if(operation ==="sub"){
+        return "-";
+    }
+    else if(operation ==="mul"){
+        return "*";
+    }
+    else if(operation ==="div"){
+        return "/";
+    }
+}
+
+function handleCalculation(){
+    if(currentOperation === null || firstValue === null) return;
+    let result;
+
+    switch(currentOperation){
+        case "add":
+            result = firstValue + Number(secondValue);
+            break;
+        case "sub":
+            result = firstValue - Number(secondValue);
+            break;
+        case "mul":
+            result = firstValue * Number(secondValue);
+            break;
+        case "div":
+            if(Number(secondValue) === 0){
+                alert("Dijeljenje s nulom nije dozvoljeno");
+                resetAfterCalculation();
+                return;
+            }
+            result = firstValue / Number(secondValue);
+            break;
+        case "square":
+            result = firstValue **2;
+            break;
+    }
+    displayEl.textContent = result;
+    resetAfterCalculation(result);
+}
+
+function resetAfterCalculation(result){
+    firstValue = result;
+    secondValue = null;
+    currentOperation = null;
+    currentValue = "";
+    expressionEl.textContent = "";
 }
 
 function clearInput(){
